1dbbc95f6baac530d891952e6e82c3460900d869
BenZalatan <bzalatan1@gmail.com>
2021-01-18 19:16:00 -0500

    allowed for variable install location amongst other very minor improvements

--- GrainBound/Program.cs
+++ GrainBound/Program.cs
@@ -7,6 +7,13 @@ namespace GrainBound
     {
         static void Main(string[] args)
         {
+            if(!System.IO.File.Exists(Environment.CurrentDirectory + "\\python\\python.exe") || !System.IO.File.Exists("GrainBoundApp_no_eds.py"))
+            {
+                Console.WriteLine("ERROR: Main python files not found! GrainBound did not install correctly. Press any key to continue...");
+                Console.ReadKey();
+                return;
+            }
+
             Process cmd = new Process();
 
             cmd.StartInfo.FileName = "cmd.exe";
@@ -18,6 +25,8 @@ namespace GrainBound
             cmd.Start();
 
             /* execute "dir" */
+
+            Console.WriteLine("Launching GrainBound process...");
 
             cmd.StandardInput.WriteLine(Environment.CurrentDirectory + "\\python\\python.exe GrainBoundApp_no_eds.py");
             cmd.StandardInput.Flush();
